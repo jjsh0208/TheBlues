67c9f24fadecf3168d6418d0876591968b54b3d9
jjsh0208 <jjsh0208@gmail.com>
2024-03-25 17:54:30 +0900

    first commit

--- src/main/java/com/theblues/theblues/Controller/MainController.java
+++ src/main/java/com/theblues/theblues/Controller/MainController.java
@@ -1,9 +1,15 @@
 package com.theblues.theblues.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
+
+    @GetMapping("/index")
+    public String index(){
+        return "layouts/index";
+    }
 
 }
